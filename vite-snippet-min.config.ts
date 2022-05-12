8c29e4b63efb07a6f341b212bba0b37cb491f99b
A.Block <3756473+zendive@users.noreply.github.com>
2022-05-12 14:59:54 +0300

    rearrange build scripts + add cloning of snippet scripts as latest

--- vite-snippet-min.config.ts
+++ vite-snippet-min.config.ts
@@ -1,8 +1,35 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
+import { copyFileSync } from 'fs';
 import { version } from './package.json';
 
+function cloneAsLatest(): PluginOption {
+  return {
+    name: 'clone-as-latest',
+    closeBundle() {
+      try {
+        copyFileSync(
+          `./dist-snippet/${version}.js`,
+          './dist-snippet/latest.js'
+        );
+        copyFileSync(
+          `./dist-snippet/${version}.min.js`,
+          './dist-snippet/latest.min.js'
+        );
+        copyFileSync(
+          `./dist-snippet/${version}.min.js.map`,
+          './dist-snippet/latest.min.js.map'
+        );
+        console.log('\n✓ iife snippet cloned as latest');
+      } catch (err: any) {
+        console.error(`\n❗️iife snippet wasn't cloned as latest`, err);
+        process.exit(err?.errno);
+      }
+    },
+  };
+}
 export default defineConfig({
+  plugins: [cloneAsLatest()],
   define: {
     __APP_VERSION__: JSON.stringify(version),
   },
